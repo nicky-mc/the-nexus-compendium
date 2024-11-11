765e2e9d18a2919899b87d1adb9e062d2f185a79
Nicky Mortoza-Cowles <cinimodazotrom@gmail.com>
2024-11-11 12:40:01 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,6 @@ export const metadata = {
   title: 'The  Nexus',
   description: 'Your D&D companion app',
 };
- main
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
