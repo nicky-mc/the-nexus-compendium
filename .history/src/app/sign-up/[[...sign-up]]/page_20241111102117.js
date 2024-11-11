9a5a9f74dc0de513dffa4f882b4824af9db77cfc
Anthony Ricketts <mra.ricketts@gmail.com>
2024-11-11 11:21:39 +0000

    added clerk but needs to be added to navbar

---
+++ .history/src/app/sign-up/[[...sign-up]]/page_20241111102117.js
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignupPage() {
+  return (
+    <>
+      <h1></h1>
+    </>
+  );
+}
---
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignupPage() {
+  return (
+    <>
+      <h1>Sign up please</h1>
+      <SignUp />
+    </>
+  );
+}
