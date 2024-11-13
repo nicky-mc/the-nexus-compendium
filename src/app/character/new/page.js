feb72d09134d4e19b812cf33bd371c199f09f36c
AlexBuck <underflux@googlemail.com>
2024-11-13 15:05:17 +0000

    sql queriy tests for the character page

--- src/app/character/new/page.js
+++ src/app/character/new/page.js
@@ -1,10 +1,17 @@
-import CreateCharacterForm from '@/app/components/CreateCharacterSandbox';
+import CreateCharacterForm from "@/app/components/CreateCharacterSandbox";
 
-
-export default function NewCharacterPage() {
+export default async function NewCharacterPage() {
+  const user =
+    await db.query`INSERT INTO test_one (player_name, info, misc_info, ac, profiecency_bonus, stats, proficiencies, skills, inventory, languages, features, hp, spell_slots, spells, notes) `;
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);
+  console.log(user);
   return (
     <div className="p-8">
       <CreateCharacterForm />
     </div>
   );
 }
+// INSERT INTO character(player_name, info, misc_info, ac, profiecency_bonus, stats, proficiencies, skills, inventory, languages, features, hp, spell_slots, spells, notes)
+// VALUES(
+// 'test',
+// ('Benedict','Human','Cleric','Acolyte','(Lawful,Good)'::ALIGNMENT_CHART,0,1) :: CHARACTER_INFO,
