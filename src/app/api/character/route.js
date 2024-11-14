ec089af2f2a1388dd6b81268c50b46405f653634
nicky-mc <cinimod@gmail.com>
2024-11-14 15:48:37 +0000

    test push

--- src/app/api/character/route.js
+++ src/app/api/character/route.js
@@ -1,4 +1,4 @@
-import { db } from "@/app/utils/dbconnection.js";
+import { db } from "@/utils/dbconnection";
 import { getAuth } from "@clerk/nextjs"; // Clerk middleware for authentication
 
 export default getAuth(async (req, res) => {
